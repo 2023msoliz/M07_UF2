6ae266bfc87c0dca2299ddf8814305763dbf2a8c
Marco Antonio Soliz Lazarte <2023_marco.soliz@iticbcn.cat>
2023-11-23 19:07:15 +0100

    commit final

---
+++ storage/framework/views/3f2f3099a177ee6964bfa8775898f3a8.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign In</title>
+</head>
+<body>
+    <h1>Iniciar Sessión</h1>
+    <form action="POST">
+        <label for="user">Usuario</label><br>
+        <input type="text" id="user"><br><br>
+        <label for="pw">Contraseña</label><br>
+        <input type="password" id="pw"><br><br>
+        <input type="button" value="Iniciar sessión">
+    </form>
+</body>
+</html><?php /**PATH /app/resources/views/signin.blade.php ENDPATH**/ ?>
